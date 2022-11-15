752ad59455f14c8817a29f8add5df7b1f307242f
Michael Lian <cafebubs@gmail.com>
2022-11-14 16:54:10 -0800

    finish lab

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 // sw.js - This file needs to be in the root of the directory to work,
 //         so do not move it next to the other scripts
 
-const CACHE_NAME = 'lab-7-starter';
+const CACHE_NAME = "lab-7-starter";
 
 // Installs the service worker. Feed it some initial URLs to cache
-self.addEventListener('install', function (event) {
+self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
@@ -15,12 +15,12 @@ self.addEventListener('install', function (event) {
 });
 
 // Activates the service worker
-self.addEventListener('activate', function (event) {
+self.addEventListener("activate", function (event) {
   event.waitUntil(self.clients.claim());
 });
 
 // Intercept fetch requests and cache them
-self.addEventListener('fetch', function (event) {
+self.addEventListener("fetch", function (event) {
   // We added some known URLs to the cache above, but tracking down every
   // subsequent network request URL and adding it manually would be very taxing.
   // We will be adding all of the resources not specified in the intiial cache
@@ -34,7 +34,21 @@ self.addEventListener('fetch', function (event) {
   /*******************************/
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
-  // B8. TODO - If the request is in the cache, return with the cached version.
-  //            Otherwise fetch the resource, add it to the cache, and return
-  //            network response.
+  event.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      // B8. TODO - If the request is in the cache, return with the cached version.
+      //            Otherwise fetch the resource, add it to the cache, and return
+      //            network response.
+      return cache.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        } else {
+          return fetch(event.request).then((fetchedResponse) => {
+            cache.put(event.request, fetchedResponse.clone());
+            return fetchedResponse;
+          });
+        }
+      });
+    })
+  );
 });
